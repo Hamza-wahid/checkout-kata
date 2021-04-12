fb38c2fbdd834f379634af8bf91339941b5d81de
Hamza Wahid <hamza@thelinkingcompany.com>
2021-04-12 03:28:11 +0100

    Checkout code + tests

---
+++ src/main/scala/checkout/Checkout.scala
@@ -0,0 +1,39 @@
+import checkout.model.{ItemPricingRule, PricingRules}
+
+class Checkout(items: Seq[Char], rules: PricingRules) {
+
+    def checkoutItems: Int = {
+      filterItems
+        .groupBy(identity)
+        .map { case (itemId, items) =>
+        val pricingRules: ItemPricingRule = rules(itemId)
+        calculateTotalForItem(items.size, pricingRules.unitPrice, pricingRules.discountQuantity, pricingRules.specialPrice)
+      }.sum
+    }
+
+    private def filterItems: Seq[Char] = {
+       val actualItems = rules.keys.toSeq
+      items.filter(actualItems.contains)
+    }
+
+    private def calculateTotalForItem(itemQuantity: Int, unitPrice: Int, discountQuantity: Int, specialPrice: Int): Int = {
+      if (discountQuantity == 0) itemQuantity * unitPrice
+      else {
+        val quantityChargedAtUnitPrice = itemQuantity % discountQuantity
+        val quantityChargedAtSpecialPrice = (itemQuantity - quantityChargedAtUnitPrice) / discountQuantity
+
+        (quantityChargedAtUnitPrice * unitPrice) + (quantityChargedAtSpecialPrice * specialPrice)
+      }
+    }
+
+  }
+
+object Checkout {
+    def apply(items: Seq[Char], rules: Map[Char, ItemPricingRule]): Int = {
+      val checkout = new Checkout(items, rules)
+      checkout.checkoutItems
+    }
+  }
+
+
+
---
+++ src/test/scala/CheckoutTestingSuite.scala
@@ -0,0 +1,61 @@
+import checkout.model.ItemPricingRule
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CheckoutTestingSuite extends AnyFlatSpec {
+
+  val pricingRules = Map(
+    'A' -> ItemPricingRule(50, 3, 130),
+    'B' -> ItemPricingRule(30, 2, 45),
+    'C' -> ItemPricingRule(50, 0, 0),
+    'D' -> ItemPricingRule(50, 0, 0),
+  )
+
+  "Checkout" should "return 0 when there are no items" in {
+    val actual = Checkout(Seq.empty, pricingRules)
+    val expected = 0
+    assert(actual === expected)
+  }
+
+  it should "return 0 for a single invalid item" in {
+    val actual = Checkout(Seq('Z'), pricingRules)
+    val expected = 0
+    assert(actual === expected)
+  }
+
+  it should "return the unit price for a single item" in {
+    val actual = Checkout(Seq('A'), pricingRules)
+    val expected = 50
+    assert(actual === expected)
+  }
+
+  it should "return the sum of unit prices for 2 single items" in {
+    val actual = Checkout(Seq('A', 'B'), pricingRules)
+    val expected = 80
+    assert(actual === expected)
+  }
+
+  it should "apply the special price correctly" in {
+    val actual = Checkout(Seq('A', 'A', 'A'), pricingRules)
+    val expected = 130
+    assert(actual === expected)
+  }
+
+  it should "charge the special price and unit price correctly" in {
+    val actual = Checkout(Seq('A', 'A', 'A', 'A'), pricingRules)
+    val expected = 180
+    assert(actual === expected)
+  }
+
+  it should "charge only the unit price for an item with no special price" in {
+    val actual = Checkout(Seq('C', 'C', 'C', 'C'), pricingRules)
+    val expected = 200
+    assert(actual === expected)
+  }
+
+  it should "charge correctly when there are multiple distinct items with varying number of discounts" in {
+    val actual = Checkout(Seq('A', 'A', 'A', 'B', 'B', 'B', 'C', 'Z'), pricingRules)
+    val expected = 255
+    assert(actual === expected)
+
+  }
+}
